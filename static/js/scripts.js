ab87b8592a98b691e4267ca6aa40913f20f2ca07
ShiyeZhu41 <373429355@qq.com>
2024-10-02 14:59:16 +0800

    202410020259
    1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'publications', 'awards']
+const section_names = ['home', 'publications', 'Invited Talk and Presentations', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
